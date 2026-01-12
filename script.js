9df999efac8cb155c4b98360a56300ebdc74f5ed
mostafamohammed985725-bit <mostafamohammed985725@gmail.com>
2026-01-12 19:06:35 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,37 @@ setTimeout(() => {
 // تحديث حالة البوت عشوائي للتجربة
 const statuses = ["متصل", "غير متصل", "مشغول", "قريبًا"];
 document.getElementById("bot-status").innerText = statuses[Math.floor(Math.random() * statuses.length)];
+function sendMessage() {
+  const input = document.getElementById("userInput");
+  const msg = input.value.trim();
+  if (!msg) return;
+
+  addMessage(msg, "user");
+  input.value = "";
+
+  let found = false;
+
+  for (let item of knowledgeBase) {
+    if (
+      item.title.includes(msg) ||
+      item.keywords.some(k => msg.includes(k))
+    ) {
+      addMessage(item.content, "bot");
+      found = true;
+      break;
+    }
+  }
+
+  if (!found) {
+    addMessage("حالياً ما عندي جواب، سيتم إضافة المعلومة قريباً 🌚", "bot");
+  }
+}
+
+function addMessage(text, type) {
+  const box = document.getElementById("hyper-messages");
+  const div = document.createElement("div");
+  div.className = type;
+  div.textContent = text;
+  box.appendChild(div);
+  box.scrollTop = box.scrollHeight;
+}
